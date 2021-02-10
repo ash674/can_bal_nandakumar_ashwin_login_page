0a1ca060eee07e42655c079a3d948b1b177208e2
ashwin <ashu61998@gmail.com>
2021-02-10 12:38:41 +0530

    Styling and php

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../load.php'; 
+confirm_logged_in();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="main.css">    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Dashboard page</h2>
+
+    <a href="admin_logout.php">Sign out</a>
+</body>
+</html>
---
+++ admin/scripts/login.php
@@ -0,0 +1,45 @@
+
+    <?php
+    function login($username, $password, $ip){
+    $pdo = Database::getInstance()->getConnection();
+    $get_user_query = 'SELECT * FROM tbl_user WHERE user_name = :username AND user_pass = :password';
+    $user_set = $pdo->prepare($get_user_query);
+    $user_set->execute(
+        array(
+         ':username'=>$username,
+        ':password'=>$password
+        )
+        );
+
+        if($found_user = $user_set->fetch(PDO::FETCH_ASSOC)){
+            $found_user_id = $found_user['user_id'];
+            $_SESSION['user_id'] = $found_user_id;
+            $_SESSION['user_name'] = $found_user['user_fname'];
+
+            $update_user_query = 'UPDATE tbl_user SET user_ip=:user_ip WHERE user_id=:user_id';
+            $update_user_set = $pdo->prepare($update_user_query);
+            $update_user_set->execute(
+                array(
+                ':user_ip'=>$ip,
+                ':user_id'=>$found_user_id
+                )
+            );
+
+            redirect_to('index.php');
+            return 'we are logging in';
+        }else{
+            return 'Wrong username or password, try again';
+        }
+
+    }
+    function confirm_logged_in(){
+        if(!isset($_SESSION['user_id'])){
+            redirect_to('admin_login.php');
+        }
+    }
+
+    function logout(){
+        session_destroy();
+        redirect_to('admin_login.php');
+    }
+    ?>
