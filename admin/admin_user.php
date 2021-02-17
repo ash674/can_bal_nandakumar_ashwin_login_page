aa0a7a1fcd5883bf2b5e13325d49a7e5257cce64
ash674 <55156277+ash674@users.noreply.github.com>
2021-02-18 00:18:48 +0530

    Update admin_user.php

--- admin/admin_user.php
+++ admin/admin_user.php
@@ -73,7 +73,7 @@ mail($to, $subject, $message, $headers);
 function passwordGenerator() {
     $all_possible_alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     $password = array();
-    $alphabetsLen = strlen($all_possible_alphabet) - 1; //put the length -1 in cache
+    $alphabetsLen = strlen($all_possible_alphabet) ; 
     for ($i = 0; $i < 8; $i++) {
         $pass_var = rand(0, $alphabetsLen);
         $password[] = $all_possible_alphabet[$pass_var];
